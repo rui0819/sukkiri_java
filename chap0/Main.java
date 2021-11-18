0728bb905da9d660a8da84f7877989da0868a40a
rui0819 <yuki.primadonna@gmail.com>
2021-11-18 14:53:54 +0900

    更新

--- chap0/Main.java
+++ chap0/Main.java
@@ -2,5 +2,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("すがわら");
 		System.out.println("31歳です。");
+		System.out.println("お酒が嫌いです。");
+		System.out.println("31+31の計算をします。");
+		System.out.println(31 + 31);
 	}
 }
