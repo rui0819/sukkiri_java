9eebab718bad4119d8875ef3cd2a4922ba2f831d
rui0819 <yuki.primadonna@gmail.com>
2021-11-19 16:31:01 +0900

    変更

--- chap02/Ex2_3.java
+++ chap02/Ex2_3.java
@@ -2,6 +2,10 @@ public class Ex2_3 {
   public static void main(String[] args) {
     System.out.println("ようこそ占いの館へ");
     System.out.println("あなたの名前を入力して下さい。");
-    String name = new java.util.Scanner(System.in).nexLine();
+    String name = new java.util.Scanner(System.in).nextLine();
+    System.out.println("あなたの年齢を入力して下さい。");
+    String ageString = new java.util.Scanner(System.in).nextLine();
+    int age = ageString
+
   }
 }
