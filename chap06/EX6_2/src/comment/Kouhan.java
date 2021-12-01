2ef90cac7d6492e814924fccade5a834d79a28d1
rui0819 <yuki.primadonna@gmail.com>
2021-12-01 12:22:55 +0900

    ６章終わり

--- chap06/EX6_2/src/comment/Kouhan.java
+++ chap06/EX6_2/src/comment/Kouhan.java
@@ -8,7 +8,6 @@ public class Kouhan {
   public static void showMondokoro() throws Exception {
     System.out.println("飛車さん、角さん。もういいでしょう。");
     System.out.println("この紋所が目にはいらぬか！");
-    thread.sleep(3000);
     Zenhan.doTogame();
   }
 
